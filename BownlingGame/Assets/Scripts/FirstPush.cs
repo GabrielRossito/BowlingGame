3dfc47a2711333e49677223ce1cd63bd163fe53b
GabrielRossito <gabrielmesquit@gmail.com>
2016-12-13 20:40:58 -0200

    Adição de forças para controle

--- BownlingGame/Assets/Scripts/FirstPush.cs
+++ BownlingGame/Assets/Scripts/FirstPush.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class FirstPush : MonoBehaviour
 {
-    [SerializeField]
-    private int _impulse = 10, _minPower = 500, _maxPower = 1500;
+    [SerializeField] private int _impulse = 10, _minPower = 500, _maxPower = 1500;
+    [SerializeField] private float _velocity = 20;
+    [SerializeField] private Text DebugValue;
 
     private Rigidbody _ballRb { get { return GetComponent<Rigidbody>(); } }
 
+    private bool _pushed;
+    private Vector3 _lastAcceleration;
+
     private void Awake()
     {
+        Input.backButtonLeavesApp = true;
+
         _ballRb.freezeRotation = true;
+        _lastAcceleration = transform.rotation.eulerAngles;
+
+    }
+    
+    void LateUpdate()
+    {
+        if (_pushed)
+            _ballRb.AddForce(new Vector3(Input.acceleration.x * _velocity, 0,0));
+        string debug = string.Empty;
+        debug += Input.acceleration + System.Environment.NewLine;
+        DebugValue.text = debug;
     }
 
     public void Push(int percentage)
     {
+        _pushed = true;
         _ballRb.freezeRotation = false;
         _ballRb.AddForce(0, 0, _impulse, ForceMode.Impulse);
         //print((_maxPower * percentage / 100));
--- BownlingGame/Assets/Scripts/PrototypeSceneManage.cs
+++ BownlingGame/Assets/Scripts/PrototypeSceneManage.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
+using UnityEngine.SceneManagement;
 
 public class PrototypeSceneManage : MonoBehaviour
 {
-    [SerializeField]
-    private FirstPush _push;
-    [SerializeField]
-    private PushBar _bar;
-    [SerializeField]
-    private CameraFallowBall _follow;
+    [SerializeField] private FirstPush _push;
+    [SerializeField] private PushBar _bar;
+    [SerializeField] private CameraFallowBall _follow;
 
     private void Start()
     {
         _bar.StartBar(BarStoped);
+    }
+
+    public void ActionRestart()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void BarStoped(int powerPercentage)
